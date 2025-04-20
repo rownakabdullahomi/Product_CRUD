a07798214680981b6d9ca1e747f62e364e6a4fdb
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-04-20 21:15:26 +0600

    create new product with product schema done

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
 import express from "express"
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
+
+app.post("/api/products", async (req, res)=>{
+    const product = req.body;
+    const newProduct = new Product(product);
+    const result = await newProduct.save();
+    res.send(result);
+})
+
+
+
 
 app.get("/", (req, res) => { 
     res.send("Server is ready !")
