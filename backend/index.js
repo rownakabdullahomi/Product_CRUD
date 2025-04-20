5cea8498aeb02ecf83a5cbf1ddcf1e480ffa85d4
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-04-21 02:42:36 +0600

    apis created for products

--- backend/index.js
+++ backend/index.js
@@ -8,17 +8,33 @@ const app = express();
 app.use(express.json());
 
 
-app.post("/api/products", async (req, res)=>{
+app.get("/api/products", async (req, res) => {
+    const result = await Product.find({});
+    res.send(result);
+})
+
+app.post("/api/products", async (req, res) => {
     const product = req.body;
     const newProduct = new Product(product);
     const result = await newProduct.save();
     res.send(result);
 })
 
+app.put("/api/product/:id", async (req, res) => {
+    const id = req.params.id;
+    const product = req.body;
+    const result = await Product.findByIdAndUpdate(id, product, {new: true});
+    res.send(result);
+})
+
+app.delete("/api/products/:id", async (req, res) => {
+    const id = req.params.id;
+    const result = await Product.findByIdAndDelete(id);
+    res.send(result);
+})
 
 
-
-app.get("/", (req, res) => { 
+app.get("/", (req, res) => {
     res.send("Server is ready !")
 })
 
