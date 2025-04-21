146a0edb383ca866804c73244f3c6572c9107cff
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-04-21 14:58:31 +0600

    navbar ongoing

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import { Button, Container, Flex, HStack, Icon, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { PlusSquareIcon } from "@chakra-ui/icons";
+import { TiShoppingCart } from "react-icons/ti";
+
+const Navbar = () => {
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+          <Link to={"/"}>
+            Product Store
+            {/* <Icon color={"black"} as={TiShoppingCart} /> */}
+          </Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <PlusSquareIcon fontSize={20} />
+            </Button>
+          </Link>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
